466a8ba022f71e85cc67213c1cd4dd4305ba9520
g-bunny <miri@newschool.edu>
2018-05-11 12:11:52 -0400

    adding session with scott & jene 5/10/18 - animation blending

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	public bool turnRight = false;
+	public bool turnLeft = false;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		
+		animator = this.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		listenForInput();
+		animator.SetBool("turnLeft", turnLeft);
+		//turnRight = false;
+		//turnLeft = false;
+	}
+
+	void listenForInput(){
+		if (Input.GetKey(KeyCode.RightArrow)){
+			turnRight = true;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			turnLeft = true;
+		}
 	}
 }
